79a4a1c6ecf90f5048d417928cbd5630401b14aa
strom87 <daniel.strom.87@gmail.com>
2015-11-15 18:02:26 +0100

    Initial Commit

---
+++ GeoLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoLocation
+//
+//  Created by Daniel Ström on 15/11/15.
+//  Copyright © 2015 Daniel Ström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
